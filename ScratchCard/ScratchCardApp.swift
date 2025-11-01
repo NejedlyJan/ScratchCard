edf765513ed910a584ced6f6c5d911b25f0b752e
Nejedly Jan <jan.nejedly@kb.cz>
2025-11-01 17:46:29 +0100

    Initial Commit

---
+++ ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by Jan Nejedlý on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
