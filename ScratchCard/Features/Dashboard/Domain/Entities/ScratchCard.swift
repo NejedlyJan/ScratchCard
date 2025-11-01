f2eac4c8b9af382fd5504cbb738d060eead2fe60
Nejedly Jan <jan.nejedly@kb.cz>
2025-11-01 18:02:06 +0100

    Add scratch card entity

---
+++ ScratchCard/Features/Dashboard/Domain/Entities/ScratchCard.swift
@@ -0,0 +1,16 @@
+//
+//  ScratchCard.swift
+//  ScratchCard
+//
+//  Created by Jan Nejedlý on 01.11.2025.
+//
+
+struct ScratchCard {
+    enum ScratchCardState {
+        case unscratched
+        case scratched(code: String)
+        case activated
+    }
+
+    let state: ScratchCardState
+}
